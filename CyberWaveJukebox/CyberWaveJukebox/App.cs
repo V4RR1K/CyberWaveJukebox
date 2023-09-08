3a1bc9b994270a984a02fde41e184c974f322390
Greg Lynskey <gclynskey@outlook.com>
2023-09-08 12:03:13 -0400

    Greg: Added preliminary C# project

---
+++ CyberWaveJukebox/CyberWaveJukebox/App.cs
@@ -0,0 +1,11 @@
+﻿
+public class App
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            Console.WriteLine("Please input valid library destination");
+        }
+    }
+}
